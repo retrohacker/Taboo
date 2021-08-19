8e8c477186f8ff295b79c4934302a69dd095bf67
William Blankenship <git@blankenship.io>
2021-08-18 19:17:44 -0700

    [server] Spin up server before DHT

---
+++ server/TabooMock.js
@@ -0,0 +1,45 @@
+const chance = require('chance')()
+const EventEmitter = require('events')
+class Taboo extends EventEmitter {
+  constructor (...opts) {
+    super(...opts)
+    // A map of infoHash -> { name, files }
+    this.TorrentTable = new Map()
+  }
+
+  addTorrent () {
+    const t = this.mockTorrent()
+    this.TorrentTable.set(t.infoHash, t)
+    this.emit('torrent', t)
+  }
+
+  mockTorrent () {
+    const infoHash = chance.hash({ length: 41 })
+    const name = chance.sentence()
+    const arrayLength = chance.integer({
+      min: 1,
+      max: 30
+    })
+    let files = new Array(arrayLength).fill(0)
+    files = files.map(() => {
+      const path = chance.sentence().replace(/ /g, '/')
+      const suffix = chance.word()
+      return path + suffix
+    })
+    return { name, files, infoHash }
+  }
+
+  mockArray () {
+    const count = chance.integer({ min: 10, max: 20 })
+    const result = new Array(count)
+    return result
+      .fill(0)
+      .map(this.mockTorrent)
+  }
+
+  torrents () {
+    return this.TorrentTable.values()
+  }
+}
+
+module.exports = Taboo
--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
 const fastify = require('fastify')()
 const ws = require('fastify-websocket')
-
+const fastifyStatic = require('fastify-static')
+const path = require('path')
 const Taboo = require('../lib')
-const taboo = new Taboo()
+let taboo
 
+console.log('Starting local server...')
 fastify.register(ws)
 
+const staticRoot = path.join(__dirname, '..', 'website', 'build')
+fastify.register(fastifyStatic, { root: staticRoot })
+
 fastify.get('/torrents', { websocket: true }, (conn) => {
+  if (!taboo) {
+    return
+  }
   const torrents = taboo.torrents()
   for (const torrent of torrents) {
-    console.log(torrent)
     conn.socket.send(JSON.stringify(torrent))
   }
   taboo.on('torrent', (torrent) => {
-    console.log(torrent)
     conn.socket.send(JSON.stringify(torrent))
   })
 })
 
 fastify.listen(3000, (e, address) => {
+  if (e) {
+    throw e
+  }
   console.log(`Server listening on ${address}`)
+  console.log('Bootstrapping DHT...')
+  taboo = new Taboo()
 })
