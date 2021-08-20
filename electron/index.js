074c0acc0557cce1e9b29aecb88ea0f44c0f53ba
William <git@blankenship.io>
2021-08-20 09:12:49 -0700

    [electron] Port webui to electron

--- electron/index.js
+++ electron/index.js
@@ -3,9 +3,13 @@ const { app, BrowserWindow } = require('electron')
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     })
-    win.loadFile('./static/index.html')
+    win.loadFile('./build/index.html')
 }
 
 app.whenReady().then(() => {
---
+++ electron/src/App.js
@@ -0,0 +1,50 @@
+/* global WebSocket */
+import React from 'react'
+import Page from './TabooUI/Page/Page.js'
+import { List } from 'immutable'
+const Taboo = window.require('TabooLib')
+let taboo
+
+// Give the UI time to bootstrap before doing
+// CPU intensive work
+setTimeout(() => {
+    taboo = new Taboo()
+}, 1000)
+
+class App extends React.PureComponent {
+  constructor () {
+    super()
+    this._checkReady = setInterval(this.checkReady.bind(this))
+    this.state = {
+        torrents: List([])
+    }
+  }
+
+  checkReady() {
+      // Once taboo is set, bootstrap the UI with all torrents
+      if(taboo) {
+          clearInterval(this._checkReady)
+          this.setState((state) => {
+              for(torrent of taboo.torrents()) {
+                  state.torrents = state.torrents.push(torrent)
+              }
+              taboo.on('torrent', this.onTorrent.bind(this))
+          })
+      }
+  }
+
+  onTorrent (torrent) {
+    // We use immutable.js to prevent unnecessary
+    // rendering. On every new torrent, append that
+    // to our existing list of torrents.
+    this.setState((state) => ({
+      torrents: this.state.torrents.push(torrent)
+    }))
+  }
+
+  render () {
+    return <Page torrents={this.state.torrents} />
+  }
+}
+
+export default App
